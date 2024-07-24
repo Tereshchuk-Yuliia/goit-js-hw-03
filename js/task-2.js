27cdd7ea04e2ecfc33c40ff9486903f5c7a78b2d
Tereshchuk-Yuliia <y.tereshchuk@icloud.com>
2024-07-24 20:56:45 +0300

    update index.html file

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,18 @@
 
+function makeArray(firstArray, secondArray, maxLength) {
+    const combinedArray = firstArray.concat(secondArray);
+    if (combinedArray.length > maxLength) {
+        return combinedArray.slice(0, maxLength);
+}  return combinedArray;
+}
+
+
+// function makeArray(firstArray, secondArray, maxLength) {
+//     const combinedArray = firstArray.concat(secondArray);
+//     if (combinedArray.Length > maxLength) {
+//         return combinedArray.slice(0, maxLength);
+//     } return combinedArray;
+// }
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
 console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
